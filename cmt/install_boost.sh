ff6312fff9063b9cad15068e4aafe349756fad8d
wsfreund <wsfreund@cern.ch>
2015-06-22 19:14:43 +0200

    Added boost automatic detection

---
+++ cmt/install_boost.sh
@@ -0,0 +1,43 @@
+#
+# Installs boost_1_58_0. It will take a while, only source this if you are sure
+# that your system does not contains the boost needed version already installed.
+#
+
+# FIXME:
+# - We may want this to check if the boost is already installed in
+# standard places and contains the python library. 
+# - We would also want this to install only the boost python library. 
+
+if ! $ROOTCOREDIR/scripts/test_cc.sh compile <<EOF
+#include <boost/version.hpp>
+#include <boost/python.hpp>
+EOF
+then
+  echo "It is needed to install boost python library." 
+  BOOST_LOCAL_PATH=$PWD
+  if test ! -f boost_1_58_0.tar.gz
+  then
+    wget http://sourceforge.net/projects/boost/files/boost/1.58.0/boost_1_58_0.tar.gz
+  fi
+  tar xfvz boost_1_58_0.tar.gz
+  cd boost_1_58_0
+  ./bootstrap.sh --prefix=$BOOST_LOCAL_PATH --with-libraries=python
+  ./b2 install --prefix=$BOOST_LOCAL_PATH --with-python
+  cd -
+  boost_include=$BOOST_LOCAL_PATH/include
+  boost_lib=$BOOST_LOCAL_PATH/lib
+  echo "test \"\${PATH#*$boost_include}\" = \"\${PATH}\" && export PATH=$boost_include:\$PATH" >> $NEW_ENV_FILE
+  echo "test \"\${LD_LIBRARY_PATH#*$boost_lib}\" = \"\${LD_LIBRARY_PATH}\" && export LD_LIBRARY_PATH=$boost_lib:\$LD_LIBRARY_PATH" >> $NEW_ENV_FILE
+  source $NEW_ENV_FILE
+if ! $ROOTCOREDIR/scripts/test_cc.sh compile <<EOF
+#include <boost/version.hpp>
+#include <boost/python.hpp>
+EOF
+then
+  echo "Couldn't install boost"
+  exit 1
+fi
+fi
+
+
+source boostsetup.sh
