e9300f1b7e141b2edf3c7bfad110fe185c5a836f
wsfreund <wsfreund@cern.ch>
2015-06-27 20:11:25 +0200

    bsub script seems to work

--- scripts/grid_submit/bsub_submit.py
+++ scripts/grid_submit/bsub_submit.py
@@ -16,46 +16,52 @@ parser.add_argument('-op','--outputPlace', action='store',
     required = True,
     help = "The output place to a lxplus tmp.")
 parser.add_argument('-n','--neurons', action='store', 
-    required = True, nargs='+', 
+    required = True, nargs='+', type=int,
     help = "Input a sequential list, the arguments should have the same format from the seq unix command.")
 parser.add_argument('-s','--nSorts', action='store', 
-    default = 50, nargs='+',
+    default = 50, type=int,
     help = "Number of cross validation sorts (kFold) ")
-parser.add_argument('-s','--queue', action='store', 
+parser.add_argument('-q','--queue', action='store', 
     default = "1nw", 
-    help = "The bjob queue. Use bqueues for a list of available queues.")
-parser.add_argument('-i','--inits', action='store', 
+    help = "The bjob queue. Use bqueues for a list of available queues (ignored if using --debug).")
+parser.add_argument('-i','--nInits', action='store', 
     default = 100, 
-    help = "The bjob queue. Use bqueues for a list of available queues.")
+    help = "The number of initializations for each sort.")
 parser.add_argument('--debug',  
     action='store_true',
-    help = "The output level for the main logger")
+    help = "Set queue to 8nm")
 import sys
 if len(sys.argv)==1:
   parser.print_help()
   sys.exit(1)
 # Retrieve parser args:
 args = parser.parse_args()
-if len(neurons) == 1:
-  neurons[1] = neurons[0]
-  neurons[0] = 0
-elif len(neurons) == 2:
-  neurons[1] = neurons[1] + 1
-elif len(neurons) == 3:
-  neurons[1] = neurons[1] + 1
+if len(args.neurons) == 1:
+  args.neurons[1] = args.neurons[0]
+  args.neurons[0] = 0
+elif len(args.neurons) == 2:
+  args.neurons[1] = args.neurons[1] + 1
+elif len(args.neurons) == 3:
+  tmp = args.neurons[1]
+  if tmp > 0:
+    args.neurons[1] = args.neurons[2] + 1
+  else:
+    args.neurons[1] = args.neurons[2] - 1
+  args.neurons[2] = tmp
 else:
-  raise ValueError("Neurons param has size greater than 3!")
+  raise ValueError("Neurons param has wrong size!")
+
+if args.debug:
+  args.queue = '8nm'
 
 from FastNetTool.util import printArgs, getModuleLogger
 logger = getModuleLogger(__name__)
-printArgs( args, logger.debug )
+printArgs( args, logger.info )
 
-# FIXME It is needed to set environment on bsub_script so that it sources the
-# new path data
-# FIXME Debug this.
-for neuron in range(*neurons):
-  for sort in range(nSorts): 
-    exec_bsub_str = """\
+import os
+for neuron in range(*args.neurons):
+  for sort in range(args.nSorts): 
+    exec_str = """\
         bsub {bsub_script}\\ 
           -q {queue} -u \"\"\\
           --datasetPlace {data} \\
@@ -66,7 +72,7 @@ for neuron in range(*neurons):
           --outputPlace {outputPlace}
         """.format(bsub_script = os.path.expandvars("$ROOTCOREBIN/user_scripts/FastNetTool/grid_submit/bsub_script.sh"),
                    queue = args.queue,
-                   datasetPlace = args.data,
+                   data = args.data,
                    neuron = neuron,
                    sort = sort,
                    inits = args.nInits,
@@ -78,6 +84,5 @@ for neuron in range(*neurons):
     exec_str = re.sub(' +',' ',exec_str)
     exec_str = re.sub('\\\\','',exec_str) # FIXME We should be abble to do this only in one line...
     exec_str = re.sub('\n','',exec_str)
-    logger.info("Command without spaces:\n%s", exec_str)
-    import os
+    #logger.info("Command without spaces:\n%s", exec_str)
     os.system(exec_str)
