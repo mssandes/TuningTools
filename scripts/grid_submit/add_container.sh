b87b3e2ab252a9210ab7ce2f3c3499d32ead4bd6
wsfreund <wsfreund@cern.ch>
2015-07-02 19:05:53 +0200

    First stable release where bsub is stable. Working on tuning on GRID environment.

---
+++ scripts/grid_submit/add_container.sh
@@ -0,0 +1,178 @@
+#!/bin/sh
+
+show_help() {
+cat << EOF
+Usage: ${0##*/} [-hv[verbosity_level=1]] 
+                -f|--file[=] INPUTFILE | -f|--file INPUTFILES
+                --dataset[=] DATASET
+                [--rse[=] RSE 'CERN-PROD_SCRATCHDISK']
+                [--useDQ2=1]
+
+Create dataset on grid containing input local files at specified rse.
+IMPORTANT: You need to have grid environment set.
+
+    -h             display this help and exit
+    -f INPUTFILES  files to upload to grid container. If one file is a directory,
+                   it will be expanded using all non diretory files inside it.
+    -v             verbose mode. Can be used multiple times for increased
+                   verbosity.
+    --dataset      the dataset name. It must be specified using
+                   user.account.datasetname.
+    --rse          The rse to upload the files and put the dataset
+    --useDQ2       If set to true, then DQ2 will be used instead of rucio.
+EOF
+}
+
+# Reset all variables that might be set
+file=''
+verbose=0 # Variables to be evaluated as shell arithmetic should be initialized to a default or validated beforehand.
+dataset=''
+rse='CERN-PROD_SCRATCHDISK'
+useDQ2=0
+
+# If no argument entered, show help and exit
+if [ $# -eq 0 ]; then
+  show_help && exit
+fi
+
+while :; do
+  case $1 in
+    -h|-\?|--help)   # Call a "show_help" function to display a synopsis, then exit.
+      show_help
+      exit
+      ;;
+    -f|--file)       # Takes an option argument, ensuring it has been specified.
+      shift
+      if [ $# -eq 0 ]; then
+        echo "ERROR: No arguments for file" >&2 && exit 1
+      fi
+      # Collect each file checking if it does not contain --
+      while :; do
+        case $1 in
+          -?*|--?*)
+            break
+            ;;
+          --)
+            break
+            ;;
+          -)
+            echo 'ERROR: Used - instead of --?' >&2
+            ;;
+          ?*)
+            if [ -d $1 ]; then
+              file="$file `find $1 -not -type d -maxdepth 1 -mindepth 1 | tr \"\n\" \" \"`"
+            else
+              file="$file $1"
+            fi
+            shift
+            ;;
+          *)
+            break
+        esac
+      done
+      continue
+      ;;
+    -f=?*|--file=?*)
+      file=${1#*=} # Delete everything up to "=" and assign the remainder.
+      ;;
+    -f=?*|--file=)         # Handle the case of an empty --file=
+      echo 'ERROR: "--file" requires a non-empty option argument.\n' >&2
+      exit 1
+      ;;
+    -v|--verbose)
+      verbose=$((verbose + 1)) # Each -v argument adds 1 to verbosity.
+      shift
+      ;;
+    -v?*)    # Set verbosity level
+      verbose=${1#-v}
+      if ! echo $verbose | egrep -q '^[0-9]+$'; then
+        echo 'ERROR: use -v[number] to set verbosity level.' >&2 && exit 1
+      fi
+      ;;
+    -v=?*|--verbose=?*)    # Set verbosity level
+      verbose=${1#*=}
+      ;;
+    --useDQ2)
+      if [ ${2#--} != $2 ]; then
+        useDQ2=1
+      else
+        useDQ2=$2
+        shift 2
+        continue
+      fi
+      ;;
+    --useDQ2=?*)
+      useDQ2=${1#*=}
+      ;;
+    --dataset)
+      if [ ${2#--} != $2 ]; then
+        echo 'ERROR: Dataset value wasn''t set.' >&2 && exit 1
+      else
+        dataset=$2
+        shift 2
+        continue
+      fi
+      ;;
+    --dataset=?*)
+      dataset=${1#*=}
+      ;;
+    --rse)
+      if [ ${2#--} != $2 ]; then
+        echo 'ERROR: rse value wasn''t set.' >&2 && exit 1
+      else
+        rse=$2
+        shift 2
+        continue
+      fi
+      ;;
+    --rse=?*)
+        rse=${1#*=}
+      ;;
+    --)              # End of all options.
+      shift
+      break
+      ;;
+    -?*)
+      echo 'WARN: Unknown option (ignored): %s\n' "$1" >&2
+      ;;
+    *)               # Default case: If no more options then break out of the loop.
+      break
+  esac
+  shift
+done
+
+# Check arguments
+test -z "$file" && echo "ERROR: option file was not specified." >&2 && show_help && exit 1;
+test $useDQ2 -gt 1 -a $useDQ2 -lt 0 && echo "ERROR: useDQ2 was set to a value different to 0 or 1." >&2 && show_help && exit 1;
+test -z $dataset && echo "ERROR: Dataset wasn't set." >&2 && show_help && exit 1;
+
+user=`echo $dataset | cut -d "." -f2`
+
+# Test 
+test -z $user && echo "ERROR: option user was not specified or couldn't be automatically detected." >&2 && exit 1;
+
+# Display some verbosity configuration:
+#test $verbose -ge 1 && echo "Files set to: $file" >&1
+test $verbose -ge 2 && echo "file is: $file" >&1
+test $verbose -ge 1 && echo "verbosity is: $verbose" >&1
+test $verbose -ge 1 && echo "User set to: $user" >&1
+test $verbose -ge 1 && echo "useDQ2 is $useDQ2" >&1
+test $verbose -ge 1 && echo "dataset is $dataset" >&1
+test $verbose -ge 1 && echo "rse is $rse" >&1
+test $verbose -ge 1 && test $useDQ2 -eq 1 && { echo "Using DQ2 creation method." >&1 || echo "Using rucio creation method." >&1; }
+
+if [ $useDQ2 -eq 0 ]; then
+  test $verbose -ge 1 && set -x
+  rucio upload --rse $rse $dataset $file 
+  test $verbose -ge 1 && set +x
+else
+  if dq2-register-dataset $dataset; then
+    if dq2-register-location $dataset $rse; then
+      if dq2-put -d -L $rse -f $files $dataset; then
+        test $verbose -ge 1 && echo "Finished uploading data." >&1
+      fi
+    fi
+  fi
+  true
+fi
+true
--- scripts/grid_submit/runGRIDtunning.py
+++ scripts/grid_submit/runGRIDtunning.py
@@ -5,76 +5,83 @@ try:
 except ImportError:
   from FastNetTool import argparse
 
-parser = argparse.ArgumentParser(description = '')
-parser.add_argument('-s','--inDS-SGN', action='store', 
-    metavar='SignalInputDataset', required = True, nargs='+',
-    help = "The signal files that will be used to tune the discriminators")
-parser.add_argument('-b','--inDS-BKG', action='store', 
-    metavar='BackgroundInputDataset', required = True, nargs='+',
-    help = "The background files that will be used to tune the discriminators")
-parser.add_argument('-o','--outDS', action='store', 
-    metavar='BackgroundInputDataset', required = True,
-    help = "The background files that will be used to tune the discriminators")
-parser.add_argument('-op','--operation', action='store', required = True, 
-    help = "The operation environment for the algorithm")
-parser.add_argument('--reference', action='store', nargs='+',
-    metavar='(BOTH | SGN BKG)_REFERENCE', default = ['Truth'], choices = ('Truth','Off_CutID','Off_Likelihood'),
-    help = """
-      The reference used for filtering datasets. It needs to be set
-      to a value on the Reference enumeration on FilterEvents file.
-      You can set only one value to be used for both datasets, or one
-      value first for the Signal dataset and the second for the Background
-      dataset.
-          """)
-import logging
-parser.add_argument('--output-level', default = logging.INFO, 
-    help = "The output level for the main logger")
-parser.add_argument('-t','--treePath', metavar='TreePath', action = 'store', 
-    default = None, type=str,
-    help = """The Tree path to be filtered on the files.
-        This argument is required if operation isn't set to Offline!!
-      """)
-parser.add_argument('--test', default = '--express --', 
-    help = "The output level for the main logger")
+parser = argparse.ArgumentParser(description = 'Run training job on grid')
+parser.add_argument('-id','--data', required = True,
+    help = "The input dataset with the data for discriminator tunning")
+parser.add_argument('-o','--outDS', required = True,
+    help = "The output dataset name.")
+parser.add_argument('-i','--inDS', 
+    metavar = 'InputDataset', 
+    help = "Input dataset to loop upon files to retrieve configuration. There will be one job for each file on this container")
+parser.add_argument('--debug',  
+    const = '--nFiles=1 --express --debugMode --allowTaskDuplication'
+    help = "Set debug options and only run 1 job.")
+import sys
+if len(sys.argv)==1:
+  parser.print_help()
+  sys.exit(1)
 # Retrieve parser args:
 args = parser.parse_args()
-# Treat special argument
-if len(args.reference) > 2:
-  raise ValueError("--reference set to multiple values: %r", args.reference)
-if len(args.reference) is 1:
-  args.reference.append( args.reference[0] )
-logger = logging.getLogger(__name__)
-logger.setLevel(args.output_level)
 
-try:
-  from pprint import pprint
-  args_dict = vars(args)
-  logger.info('Retrieved the following configuration:')
-  pprint([(key, args_dict[key]) for key in sorted(args_dict.keys())])
-except ImportError:
-  logger.info('Retrieved the following configuration: \n %r', vars(args))
+from FastNetTool.util import printArgs, getModuleLogger
+logger = getModuleLogger(__name__)
+printArgs( args, logger.info )
 
 import os
-os.system("""
-            prun --exec "gridCreateData --sgnInputFiles %IN2
-                                        --bkgInputFiles %IN3" 
-                 --secondaryDSs=IN2:{inDS_SGN},IN3:{inDS_BKG} 
-                 --outDS={outDS}
-                 --inTarBall={inTarBall}
-                 --useRootCore
-                 --disableAutoRetry
-                 --noCompile
-                 %{extraFlags}
-          """.format(inDS_SGN=args.inDS_SGN,
-                     inDS_BKG=args.inDS_BKG,
-                     outDS=args.outDS,
-                     inTarBall=args.inTarBall,
-                     extraFlags=args.test))
-createData( sgnFileList = args.sgnInputFiles, 
-            bkgFileList = args.bkgInputFiles,
-            ringerOperation = args.operation,
-            referenceSgn = args.reference[0],
-            referenceBkg = args.reference[1],
-            treePath = args.treePath,
-            output = args.output)
+#os.system('rcSetup -u')
+exec_str = """\
+      i bsub -q {queue} -u \"\" -J pyTrain \\
+        {bsub_script} \\ 
+          --jobConfig {jobFile} \\
+          --datasetPlace {data} \\
+          --output {output} \\
+          --outputPlace {outputPlace} \\
+    """.format(bsub_script = os.path.expandvars("$ROOTCOREBIN/user_scripts/FastNetTool/grid_submit/bsub_script.sh"),
+               queue = args.queue,
+               data = args.data,
+               jobFile = f,
+               output = args.output,
+               outputPlace = args.outputPlace,
+               )
 
+exec_str = """\
+            prun --exec "./ --sgnInputFiles %IN \\
+                                          --bkgInputFiles %BKG \\
+                                          --operation {operation} \\
+                                          --output fastnet.pic \\
+                                          --reference {referenceSgn} {referenceBkg} \\
+                                          {treePath} " \\
+                 --inDS={inDS_SGN} \\
+                 --nFilesPerJob={nSgnFiles} \\
+                 --nFiles={nSgnFiles} \\
+                 --nJobs=1 \\
+                 --secondaryDSs=BKG:{nBkgFiles}:{inDS_BKG}  \\
+                 --outDS={outDS} \\
+                 {tarBallArg} \\
+                 --site=ANALY_BNL_SHORT,ANALY_BNL_LONG \\
+                 --useRootCore \\
+                 --disableAutoRetry \\
+                 --outputs=fastnet.pic \\
+                 --maxNFilesPerJob={nInputFiles} \\
+                 --nGBPerJob=10000 \\
+                 {extraFlags}
+          """.format(inDS_SGN='%s' % ' '.join(args.inDS_SGN),
+                     inDS_BKG='%s' % ' '.join(args.inDS_BKG),
+                     operation=args.operation,
+                     outDS=args.outDS,
+                     nSgnFiles=nSgnFiles, # it seems that setting nJobs=1 doesn't work... but why?
+                     nBkgFiles=nBkgFiles,
+                     nInputFiles=nSgnFiles+nBkgFiles,
+                     tarBallArg=conditionalOption('--inTarBall=', args.inTarBall) + conditionalOption('--outTarBall=', args.outTarBall),
+                     referenceSgn=args.reference[0],
+                     referenceBkg=args.reference[1],
+                     treePath = conditionalOption('--treePath ', args.treePath),
+                     extraFlags = args.debug if args.debug else '--skipScout',
+                     )
+logger.info("Executing following command:\n%s", exec_str)
+import re
+exec_str = re.sub(' +',' ',exec_str)
+exec_str = re.sub('\\\\','',exec_str) # FIXME We should be abble to do this only in one line...
+exec_str = re.sub('\n','',exec_str)
+#logger.info("Command without spaces:\n%s", exec_str)
+os.system(exec_str)
