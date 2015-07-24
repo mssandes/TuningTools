56c540225daa3d5880227457d68acfdeb30022c2
Werner Freund <wsfreund@gmail.com>
2015-07-24 21:59:34 +0200

    Fixed boost installation

--- cmt/install_boost.sh
+++ cmt/install_boost.sh
@@ -22,8 +22,8 @@ then
     echo "It is needed to install boost python library." 
     test \! -f boost_1_58_0.tar.gz && { wget http://sourceforge.net/projects/boost/files/boost/1.58.0/boost_1_58_0.tar.gz || \
       { echo "Couldn't download boost!" && exit 1; } }
-    test \! -e boost_1_58_0 && { echo -n "Extracting files..." && tar xfz boost_1_58_0.tar.gz && echo " done!"; } || \
-      { echo "Couldn't extract files!" && exit 1; }
+    test \! -e boost_1_58_0 && { { echo -n "Extracting files..." && tar xfz boost_1_58_0.tar.gz && echo " done!"; } || \
+      { echo "Couldn't extract files!" && exit 1; } }
     echo "Installing boost..."
     cd boost_1_58_0
     if ./bootstrap.sh --prefix=`eval echo "$BOOST_LOCAL_PATH"` --with-libraries=python > /dev/null
