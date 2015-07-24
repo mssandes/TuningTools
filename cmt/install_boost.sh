e7fb964f75a54bd1a8f9baf10bc81f117e5103b5
wsfreund <wsfreund@cern.ch>
2015-07-24 21:32:58 +0200

    Working on boost compilation

--- cmt/install_boost.sh
+++ cmt/install_boost.sh
@@ -20,7 +20,7 @@ then
   if ! $CXX $PYTHON_INCLUDE -P boost_test.h > /dev/null 2> /dev/null
   then
     echo "It is needed to install boost python library." 
-    test \! -f boost_1_58_0.tar.gz && { wget http://sourceforge.net/projects/boost/files/boost/1.58.0/boost_1_58_0.tar.gz; || \
+    test \! -f boost_1_58_0.tar.gz && { wget http://sourceforge.net/projects/boost/files/boost/1.58.0/boost_1_58_0.tar.gz || \
       { echo "Couldn't download boost!" && exit 1; } }
     test \! -e boost_1_58_0 && { echo -n "Extracting files..." && tar xfz boost_1_58_0.tar.gz && echo " done!"; } || \
       { echo "Couldn't extract files!" && exit 1; }
