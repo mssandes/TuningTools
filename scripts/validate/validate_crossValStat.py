f769bc69c7bde788d6efcf13cbb3c09e73923d5f
joaoVictorPinto <jodafons@cern.ch>
2015-08-30 00:04:56 +0200

    las version for crossvalidstat. make possible to convert to matlab

--- scripts/validate/validate_crossValStat.py
+++ scripts/validate/validate_crossValStat.py
@@ -1,36 +1,20 @@
 
 
-from FastNetTool.util            import include, normalizeSumRow, reshape, load
 from FastNetTool.CrossValidStat  import *
-import numpy as np
-from FastNetTool.CrossValidStat  import CrossValidStat
 import os
 
-dirtouse='./networks/'
-
-DatasetLocationInput = '/afs/cern.ch/work/j/jodafons/public/mc14_13TeV.147406.129160.sgn.offCutID.bkg.truth.trig.e24_medium_L1EM20VH.npy'
-
-from FastNetTool.Logger import Logger
-mainLogger = Logger.getModuleLogger(__name__)
-
-mainLogger.info('Opening data...')
-objDataFromFile                   = np.load( DatasetLocationInput )
-Data                              = normalizeSumRow( reshape( objDataFromFile[0] ) )
-Target                            = reshape(objDataFromFile[1])
-nSorts                            = 50
-neurons                           = [5,20]
-
-stat = CrossValidStat( Cross=Cross, Data=Data,
-                                    Target=Target,
-                                    nSorts=nSorts,
-                                    neurons=neurons,
+basepath = '/afs/cern.ch/work/j/jodafons/public'
+data ='user_jodafons_nn_mc14_13TeV_147406_129160_sgn_Off_CitID_bkg_truth_e24_medium_L1EM20VH_t0008_fastnet_tunedXYZ/'
+dirtouse=basepath+'/'+data
 finallist=[]
+
 while( dirtouse.endswith('/') ) :
   dirtouse= dirtouse.rstrip('/')
   listfiles=os.listdir(dirtouse)
   for ll in listfiles:
     finallist.append(dirtouse+'/'+ll)
+print len(finallist)
 
-stat = CrossValidStat( inputFiles=finallist )
-stat(prefix='fig', criteria=0)
+stat = CrossValidStatAnalysis( finallist,[5,20], 50 )
+stat('sp')
 
