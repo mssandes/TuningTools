c553bc7a30165f149d469d3bf8dab4e0161614d4
wsfreund <wsfreund@cern.ch>
2016-03-24 01:41:13 +0100

    Finished improving exporting discriminators for L2 operation.

--- scripts/analysis_scripts/Trigger_20_7_3_6_FixET/export_online.py
+++ scripts/analysis_scripts/Trigger_20_7_3_6_FixET/export_online.py
@@ -8,44 +8,45 @@ from pprint import pprint
 
 crossValGrid = expandFolders('$ROOTCOREBIN/../TuningTools/scripts/skeletons/','FixET_Norm1*.pic')
 
-#configList = [
-#                # EFCalo, Et 0, Eta 0
-#                #  Pd, SP, Pf
-#              [[   7,   7,  7   ],
-#               [# Et 0, Eta 1
-#                   16,  16,  7  ],
-#               [# Et 0, Eta 2
-#                   7,  19,  6   ],
-#               [# Et 0, Eta 3
-#                  5,   11, 14   ]],
-#                # EFCalo, Et 1, Eta 0
-#                #  Pd, SP, Pf
-#              [[   9,  10, 7    ],
-#               [# Et 1, Eta 1
-#                   8,  16, 7    ],
-#               [# Et 1, Eta 2
-#                   11, 11, 6    ],
-#               [# Et 1, Eta 3
-#                   8,  6,  6    ]],
-#                # EFCalo, Et 2, Eta 0
-#                #  Pd, SP, Pf
-#              [[   5,  14, 14   ],
-#               [# Et 2, Eta 1
-#                   11, 11, 11   ],
-#               [# Et 2, Eta 2
-#                   8,  9,  13   ],
-#               [# Et 2, Eta 3
-#                   15, 15, 20   ]],
-#                # EFCalo, Et 3, Eta 0
-#                #  Pd, SP, Pf
-#              [[   10, 8,  16   ],
-#               [# Et 3, Eta 1
-#                   17, 7,  7    ],
-#               [# Et 3, Eta 2
-#                   8,  17, 15   ],
-#               [# Et 3, Eta 3
-#                  16,  16,  5  ]]
-#            ]
+#configMap = [
+#              # EFCalo, Et 0, Eta 0
+#              #  Pd, SP, Pf
+#           [[   7, 7, 7   ],
+#             [# Et 0, Eta 1
+#                16,16,16  ],
+#             [# Et 0, Eta 2
+#                 19,07,19   ],
+#             [# Et 0, Eta 3
+#                14, 11,  11   ]],
+#              # EFCalo, Et 1, Eta 0
+#              #  Pd, SP, Pf
+#            [[   7,  10, 7   ],
+#             [# Et 1, Eta 1
+#                 8, 10, 20   ],
+#             [# Et 1, Eta 2
+#                 11, 11, 8   ],
+#             [# Et 1, Eta 3
+#                 11,  7, 17   ]],
+#              # EFCalo, Et 2, Eta 0
+#              #  Pd, SP, Pf
+#            [[   14,  20,  14   ],
+#             [# Et 2, Eta 1
+#                 13,  11,  11   ],
+#             [# Et 2, Eta 2
+#                 9,  9,  19   ],
+#             [# Et 2, Eta 3
+#                 20,  20,  15   ]],
+#              # EFCalo, Et 3, Eta 0
+#              #  Pd, SP, Pf
+#            [[   20, 20,  20   ],
+#             [# Et 3, Eta 1
+#                 11,  7,  7   ],
+#             [# Et 3, Eta 2
+#                 17,  17,  19   ],
+#             [# Et 3, Eta 3
+#                 16,  16,  16   ]]
+#                               ]
+#
 #
 #refBenchmarkList = [["Medium_LH_EFCalo_Pd","Medium_MaxSP","Medium_LH_EFCalo_Pf"]]
 #
@@ -54,42 +55,43 @@ crossValGrid = expandFolders('$ROOTCOREBIN/../TuningTools/scripts/skeletons/','F
 #            'e24_lhmedium_L1EM20VH_L2EFCalo_ringer_pf']
 
 configList = [
-                # EFCalo, Et 0, Eta 0
-              [[ 7   ],
-               [# Et 0, Eta 1
-                 7  ],
-               [# Et 0, Eta 2
-                 19 ],
-               [# Et 0, Eta 3
-                 5 ]],
-                # EFCalo, Et 1, Eta 0
-                #  Pd, SP, Pf
-              [[  7    ],
-               [# Et 1, Eta 1
-                  7    ],
-               [# Et 1, Eta 2
-                  11],
-               [# Et 1, Eta 3
-                  6    ]],
-                # EFCalo, Et 2, Eta 0
-                #  Pd, SP, Pf
-              [[  14   ],
-               [# Et 2, Eta 1
-                  11],
-               [# Et 2, Eta 2
-                   8   ],
-               [# Et 2, Eta 3
-                   15  ]],
-                # EFCalo, Et 3, Eta 0
-                #  Pd, SP, Pf
-              [[   16   ],
-               [# Et 3, Eta 1
-                   7    ],
-               [# Et 3, Eta 2
-                   8    ],
-               [# Et 3, Eta 3
-                  5  ]]
-            ]
+              # EFCalo, Et 0, Eta 0
+              #  Pd, SP, Pf
+           [[   7   ],
+             [# Et 0, Eta 1
+                16  ],
+             [# Et 0, Eta 2
+                 7   ],
+             [# Et 0, Eta 3
+                14   ]],
+              # EFCalo, Et 1, Eta 0
+              #  Pd, SP, Pf
+            [[   7   ],
+             [# Et 1, Eta 1
+                 20   ],
+             [# Et 1, Eta 2
+                 11   ],
+             [# Et 1, Eta 3
+                 17   ]],
+              # EFCalo, Et 2, Eta 0
+              #  Pd, SP, Pf
+            [[   14   ],
+             [# Et 2, Eta 1
+                 11   ],
+             [# Et 2, Eta 2
+                 9   ],
+             [# Et 2, Eta 3
+                 20   ]],
+              # EFCalo, Et 3, Eta 0
+              #  Pd, SP, Pf
+            [[   20   ],
+             [# Et 3, Eta 1
+                 7   ],
+             [# Et 3, Eta 2
+                 17   ],
+             [# Et 3, Eta 3
+                 16   ]]
+                               ]
 
 
 refBenchmarkList = [[ # Et 0, Eta 0
