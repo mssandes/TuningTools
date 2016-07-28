9f73ffcf81015d002d2eda960a4a4a4154f8621a
wsfreund <wsfreund@cern.ch>
2016-07-28 08:05:21 +0200

    Added script for comparing time between branches

---
+++ scripts/validate/measure_time.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+import cProfile, pstats, StringIO
+pr = cProfile.Profile()
+pr.enable()
+from TuningTools import TunedDiscrArchieve
+a  = TunedDiscrArchieve('/afs/cern.ch/work/j/jodafons/public/Tuning201603XX/user.wsfreund.nn.norm1.newstop.mc14_13TeV.147406.129160.sgn.offLH.bkg.truth.trig.ef.e24_lhmedium_nod0_tunedDiscrXYZ.tgz/user.wsfreund.8194608._000995.tunedDiscrXYZ.tgz').__enter__()
+pr.disable()
+s = StringIO.StringIO()
+sortby = 'cumulative'
+ps = pstats.Stats(pr, stream=s).sort_stats(sortby)
+ps.print_stats()
+print s.getvalue()
