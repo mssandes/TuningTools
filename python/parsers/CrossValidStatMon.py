0affa5769185e1516660b9ef1fbf6ead970abe24
joaoVictorPinto <jodafons@cern.ch>
2016-07-29 18:43:05 +0200

    harmonization names

--- python/parsers/CrossValidStatMon.py
+++ python/parsers/CrossValidStatMon.py
@@ -18,7 +18,7 @@ reqArgs.add_argument('-f', '--file', action='store', required = True,
                      help = """The crossvalidation data files or folders that will be used to run the
                                analysis.""")
 
-reqArgs.add_argument('-p','--perfFile', default = None, required = True,
+reqArgs.add_argument('-r','--refFile', default = None, required = True,
                      help = """The performance file to retrieve the operation points.""")
 
 optArgs = crossValStatsMonParser.add_argument_group( "Optional arguments", "")
