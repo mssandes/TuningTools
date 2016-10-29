ca0ccd0b9535700eb76e20357385e419502c524d
Joao Victor da Fonseca Pinto <jodafons@cern.ch>
2016-10-29 10:19:58 -0200

    separate crossval into a module

---
+++ python/crossValidation/__init__.py
@@ -0,0 +1,11 @@
+from . import CrossValid
+__all__.extend( CrossValid.__all__ )
+from .CrossValid import *
+from . import CrossValidStat
+__all__.extend( CrossValidStat.__all__ )
+from .CrossValidStat import *
+from . import SubsetGenerator
+__all__.extend( SubsetGenerator.__all__ )
+from .SubsetGenerator import *
+
+
