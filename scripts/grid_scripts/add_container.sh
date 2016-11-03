ec0f558ff06596ae2d7c2c82d1fedd92e861a07f
wsfreund <wsfreund@cern.ch>
2016-11-03 21:12:31 +0100

    Removed bug where dataset scope logic was inverted.

--- scripts/grid_scripts/add_container.sh
+++ scripts/grid_scripts/add_container.sh
@@ -194,11 +194,11 @@ test $verbose -ge 0 && rucio_verbose="--verbose"
 
 # Run command with extracted values:
 if [ $useDQ2 -eq 0 ]; then
-  if test "${dataset/:}" != "${dataset}"; then
+  if test "${dataset/:}" = "${dataset}"; then
     dataset="user.$user:$dataset"
   fi
   rucio add-dataset $dataset
-  rucio $rucio_verbose upload --rse $rse "$dataset" "$file"
+  rucio $rucio_verbose upload "$dataset" "$file" --rse $rse
   #scoped_files=$(for f in $file; do echo "user.$user:$(basename $f)"; done)
   #rucio attach ${dataset} $scoped_files
   #rucio close user.$user:$dataset
