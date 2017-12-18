974449bb131f917c4e1b18b0f1996e7bdfcba5ed
Werner Freund <wsfreund@gmail.com>
2017-12-18 03:52:14 -0200

    Removed auto import of ROOT dependent modules

--- python/monitoring/__init__.py
+++ python/monitoring/__init__.py
@@ -1,16 +1,16 @@
 __all__ = []
 
-from . import MonitoringHelper
-__all__.extend( MonitoringHelper.__all__           )
-from .MonitoringHelper import *
+#from . import MonitoringHelper
+#__all__.extend( MonitoringHelper.__all__           )
+#from .MonitoringHelper import *
 
 from . import MonitoringTool
 __all__.extend( MonitoringTool.__all__           )
 from .MonitoringTool import *
 
-from . import PlotHelper
-__all__.extend( PlotHelper.__all__           )
-from .PlotHelper import *
+#from . import PlotHelper
+#__all__.extend( PlotHelper.__all__           )
+#from .PlotHelper import *
 
 
 
