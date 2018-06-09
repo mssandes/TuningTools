3ae18b08d0cf7644adf32a489d5422a92f032924
Joao Victor da Fonseca Pinto <jodafons@cern.ch>
2018-06-09 09:44:25 -0300

    update v9 scripts

--- scripts/analysis_scripts/official/Trigger_201801XX_data17_v9/create_tuningFiles.py
+++ scripts/analysis_scripts/official/Trigger_201801XX_data17_v9/create_tuningFiles.py
@@ -1,18 +1,18 @@
 
 
-#from TuningTools.CreateTuningJobFiles import createTuningJobFiles
-#createTuningJobFiles( outputFolder   = 'config.n5to10.JK.inits_10by10',
-#                      neuronBounds   = [5,10],
-#                      sortBounds     = 10,
-#                      nInits         = 100,
-#                      nNeuronsPerJob = 1,
-#                      nInitsPerJob   = 10,
-#                      nSortsPerJob   = 1,
-#                      prefix         = 'job_slim',
-#                      compress       = True )
-#
-#
-#
+from TuningTools.CreateTuningJobFiles import createTuningJobFiles
+createTuningJobFiles( outputFolder   = 'configs.n5to10.jk.inits_20by20',
+                      neuronBounds   = [5,10],
+                      sortBounds     = 10,
+                      nInits         = 100,
+                      nNeuronsPerJob = 1,
+                      nInitsPerJob   = 20,
+                      nSortsPerJob   = 1,
+                      prefix         = 'job_config_slim',
+                      compress       = True )
+
+
+
 #from TuningTools.CrossValid import CrossValid, CrossValidArchieve
 #crossValid = CrossValid(nSorts = 10,
 #                        nBoxes = 10,
@@ -24,11 +24,11 @@
 #                            ).save( True )
 
 
-from TuningTools.PreProc import *
-ppCol = PreProcChain( Norm1() ) 
+#from TuningTools.PreProc import *
+#ppCol = PreProcChain( Norm1() ) 
 
 
-from TuningTools.TuningJob import fixPPCol
-ppCol = fixPPCol(ppCol)
-place = PreProcArchieve( 'ppFile', ppCol = ppCol ).save()
+#from TuningTools.TuningJob import fixPPCol
+#ppCol = fixPPCol(ppCol)
+#place = PreProcArchieve( 'ppFile', ppCol = ppCol ).save()
 
