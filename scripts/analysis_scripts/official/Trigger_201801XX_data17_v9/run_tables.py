424a4633f4d610ef4c971a5f2b28e652bcd635ac
Joao Victor da Fonseca Pinto <jodafons@cern.ch>
2018-06-17 12:57:13 -0300

    update scripts

--- scripts/analysis_scripts/official/Trigger_201801XX_data17_v9/run_tables.py
+++ scripts/analysis_scripts/official/Trigger_201801XX_data17_v9/run_tables.py
@@ -2,22 +2,38 @@
 
 from TuningTools.monitoring import MonitoringTool
 
-basepaths = 'data/monitoring'
-
-summaryList =  [
-            'report_tight/summary.pic.gz',
-          ]
-
+basepaths = 'data/reports'
+dataLocation = 'data_jpsi/files/data_entries.pic.gz'
 cnames = [
             '$ref$',
             '$NN_{v9}(rings)$',
           ]
 
-dataLocation = 'data_jpsi/files/data_entries.pic.gz'
 
+summaryList =  [
+            'report_tight/summary.pic.gz',
+          ]
 
 MonitoringTool.compareTTsReport(cnames,summaryList,dataLocation,title='Comparison (ref Vs v9) Tight', outname='comparison_ref_v9_tight') 
 
+summaryList =  [
+            'report_medium/summary.pic.gz',
+          ]
+
+MonitoringTool.compareTTsReport(cnames,summaryList,dataLocation,title='Comparison (ref Vs v9) Medium', outname='comparison_ref_v9_medium') 
+
+summaryList =  [
+            'report_loose/summary.pic.gz',
+          ]
+
+MonitoringTool.compareTTsReport(cnames,summaryList,dataLocation,title='Comparison (ref Vs v9) Loose', outname='comparison_ref_v9_loose') 
+
+
+summaryList =  [
+            'report_vloose/summary.pic.gz',
+          ]
+
+MonitoringTool.compareTTsReport(cnames,summaryList,dataLocation,title='Comparison (ref Vs v9) VeryLoose', outname='comparison_ref_v9_vloose') 
 
 
 
