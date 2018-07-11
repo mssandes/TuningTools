fce430447aa45168708653c4d1a96a5d05211792
Joao Victor da Fonseca Pinto <jodafons@cern.ch>
2018-07-11 17:15:52 -0300

    rename v9 dir

---
+++ scripts/analysis_scripts/official/Trigger_201807XX_data17_v9/run_reports.sh
@@ -0,0 +1,6 @@
+
+crossValStatMonAnalysis.py -f data_jpsi/crossval/crossval_tight  -d data_jpsi/files/data_entries.pic.gz  -o report_tight  --grid
+crossValStatMonAnalysis.py -f data_jpsi/crossval/crossval_medium -d data_jpsi/files/data_entries.pic.gz  -o report_medium --grid
+crossValStatMonAnalysis.py -f data_jpsi/crossval/crossval_loose  -d data_jpsi/files/data_entries.pic.gz  -o report_loose  --grid
+crossValStatMonAnalysis.py -f data_jpsi/crossval/crossval_loose  -d data_jpsi/files/data_entries.pic.gz  -o report_vloose --grid
+
