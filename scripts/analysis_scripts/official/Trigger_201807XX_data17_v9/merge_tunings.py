c296357a8c03fcfabda0cebba5fa7c2728115363
Joao Victor da Fonseca Pinto <jodafons@cern.ch>
2018-07-28 20:36:54 -0300

    add merge path to v9 workspace

--- scripts/analysis_scripts/official/Trigger_201807XX_data17_v9/merge_tunings.py
+++ scripts/analysis_scripts/official/Trigger_201807XX_data17_v9/merge_tunings.py
@@ -11,16 +11,14 @@ configs = ['Tight','Medium','Loose','VeryLoose']
 
 for pid in configs:
   files = [
-          #'data_jpsi/export/data17_20180125_v8/TrigL2CaloRingerElectron%sConstants.root'%pid,
           'data/export/jpsi/TrigL2CaloRingerElectron%sConstants.root'%pid,
           'data/export/zee/TrigL2CaloRingerElectron%sConstants.root'%pid,
         ]
   export.merge_weights( files, 'TrigL2CaloRingerElectron%sConstants'%pid )
 
   files = [
-          #'data_jpsi/export/data17_20180125_v8/TrigL2CaloRingerElectron%sThresholds.root'%pid,
-          'data/export/jpsi/TrigL2CaloRingerElectron%sThresholds.root'%pid,
-          'data/export/zee/TrigL2CaloRingerElectron%sThresholds.root'%pid,
+          'data/correction/correction_jpsi/TrigL2CaloRingerElectron%sThresholds.root'%pid,
+          'data/correction/correction_zee/TrigL2CaloRingerElectron%sThresholds.root'%pid,
         ]
   export.merge_thresholds( files, 'TrigL2CaloRingerElectron%sThresholds'%pid )
 
