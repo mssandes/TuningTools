2fc6b8dc8801277c2f1a149cf20d305010973e93
Joao Victor da Fonseca Pinto <jodafons@cern.ch>
2018-08-03 16:44:31 -0300

    update

--- scripts/analysis_scripts/official/Trigger_201807XX_data17_v9/merge_tunings.py
+++ scripts/analysis_scripts/official/Trigger_201807XX_data17_v9/merge_tunings.py
@@ -17,7 +17,7 @@ for pid in configs:
   #export.merge_weights( files, 'TrigL2CaloRingerElectron%sConstants'%pid )
 
   files = [
-          'data/correction/correction_jpsi/TrigL2CaloRingerElectron%sThresholds.root'%pid,
+          'data/correction/correction_jpsiee/TrigL2CaloRingerElectron%sThresholds.root'%pid,
           'data/correction/correction_zee/TrigL2CaloRingerElectron%sThresholds.root'%pid,
         ]
   export.merge_thresholds( files, 'TrigL2CaloRingerElectron%sThresholds'%pid )
